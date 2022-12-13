7a64b479f32b148e7815e8720116ea8ba5e98b7e
cdandoy <cedric@dandoy.org>
2022-12-13 05:00:26 -0600

    Replace the Micronaut banner

--- dbpopd/src/main/java/org/dandoy/dbpopd/Application.java
+++ dbpopd/src/main/java/org/dandoy/dbpopd/Application.java
@@ -1,17 +1,60 @@
 package org.dandoy.dbpopd;
 
 import io.micronaut.runtime.Micronaut;
-import io.swagger.v3.oas.annotations.*;
-import io.swagger.v3.oas.annotations.info.*;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.Getter;
+import lombok.SneakyThrows;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 
 @OpenAPIDefinition(
-    info = @Info(
-            title = "dbpopd",
-            version = "0.0"
-    )
+        info = @Info(
+                title = "dbpopd",
+                version = "0.0.6"
+        )
 )
 public class Application {
+
+    @Getter
+    private static long startTimeMilis;
+
     public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
+        startTimeMilis = System.currentTimeMillis();
+        System.out.println("""
+                 _____  _     _____
+                |  __ \\| |   |  __ \\
+                | |  | | |__ | |__) |__  _ __
+                | |  | | '_ \\|  ___/ _ \\| '_ \\
+                | |__| | |_) | |  | (_) | |_) |
+                |_____/|_.__/|_|   \\___/| .__/
+                                        | |
+                                        |_|""");
+        String version = getVersion();
+        if (version != null) {
+            System.out.printf(" DbPop (v%s)\n%n", version);
+        }
+
+        Micronaut.build(args)
+                .banner(false)
+                .deduceEnvironment(false)
+                .start();
+    }
+
+    @SneakyThrows
+    private static String getVersion() {
+        Properties properties = new Properties();
+        try (InputStream inputStream = Application.class.getResourceAsStream("/version.txt")) {
+            if (inputStream != null) {
+                try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                    properties.load(bufferedReader);
+                }
+            }
+        }
+        return properties.getProperty("app.version");
     }
 }
