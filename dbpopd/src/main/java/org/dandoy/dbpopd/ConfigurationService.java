4c798478cb94036fb6057d32abdeaae99e3c78f1
cdandoy <cedric@dandoy.org>
2022-12-14 06:53:36 -0600

    Use variables for sensitive data Fixes #22

--- dbpopd/src/main/java/org/dandoy/dbpopd/ConfigurationService.java
+++ dbpopd/src/main/java/org/dandoy/dbpopd/ConfigurationService.java
@@ -3,7 +3,6 @@ package org.dandoy.dbpopd;
 import io.micronaut.context.annotation.Property;
 import jakarta.inject.Singleton;
 import lombok.Getter;
-import lombok.SneakyThrows;
 import org.dandoy.dbpop.download.Downloader;
 import org.dandoy.dbpop.upload.Populator;
 
@@ -25,41 +24,35 @@ public class ConfigurationService {
     public ConfigurationService(
             @SuppressWarnings("MnInjectionPoints") @Property(name = "dbpopd.configuration.path") String configurationPath
     ) {
-        File configurationFile = getConfigurationFile(configurationPath);
+        File configurationDir = new File(configurationPath);
+        File configurationFile = new File(configurationDir, PROP_FILE_NAME);
         Properties properties = getConfigurationProperties(configurationFile);
-        jdbcurl = getValidProperty(configurationFile, properties, "jdbcurl");
-        username = getValidProperty(configurationFile, properties, "username");
-        password = getValidProperty(configurationFile, properties, "password");
-        File configurationDir = configurationFile.getParentFile();
+        jdbcurl = getValidProperty(configurationFile, properties, "jdbcurl", "DBPOP_JDBCURL");
+        username = getValidProperty(configurationFile, properties, "username", "DBPOP_USERNAME");
+        password = getValidProperty(configurationFile, properties, "password", "DBPOP_PASSWORD");
         datasetsDirectory = new File(configurationDir, "datasets");
     }
 
-    private static String getValidProperty(File configurationFile, Properties properties, String name) {
-        String value = properties.getProperty(name);
-        if (value == null) throw new RuntimeException("Missing property %s in %s".formatted(name, configurationFile));
-        return value;
+    private static String getValidProperty(File configurationFile, Properties properties, String propertyName, String environmentVariableName) {
+        String environmentValue = System.getenv(environmentVariableName);
+        if (environmentValue != null) return environmentValue;
+
+        String value = properties.getProperty(propertyName);
+        if (value != null) return value;
+
+        throw new RuntimeException("Missing environment variable %s or property %s in %s".formatted(environmentVariableName, propertyName, configurationFile));
     }
 
-    @SneakyThrows
     private static Properties getConfigurationProperties(File configurationFile) {
         Properties properties = new Properties();
-        try (BufferedReader bufferedReader = Files.newBufferedReader(configurationFile.toPath())) {
-            properties.load(bufferedReader);
+        if (configurationFile.canRead()) {
+            try (BufferedReader bufferedReader = Files.newBufferedReader(configurationFile.toPath())) {
+                properties.load(bufferedReader);
+            } catch (IOException e) {
+                throw new RuntimeException("Cannot read " + configurationFile, e);
+            }
         }
         return properties;
-    }
-
-    private static File getConfigurationFile(String configurationPath) {
-        try {
-            File configurationDir = new File(configurationPath).getCanonicalFile();
-            File configurationFile = new File(configurationDir, PROP_FILE_NAME);
-            if (!configurationFile.canRead()) {
-                throw new RuntimeException("Cannot read the configuration file: %s".formatted(configurationFile));
-            }
-            return configurationFile;
-        } catch (IOException e) {
-            throw new RuntimeException("Invalid configuration path: %s".formatted(configurationPath), e);
-        }
     }
 
     public Populator createPopulator() {
