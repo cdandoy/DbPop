61dbf227755d66126dc63c2510522733ac6d13e2
cdandoy <cedric@dandoy.org>
2023-01-07 06:22:22 -0600

    v0.1.2 release Updating the version numbers to 0.1.2

--- dbpopd/src/main/java/org/dandoy/dbpopd/Application.java
+++ dbpopd/src/main/java/org/dandoy/dbpopd/Application.java
@@ -15,7 +15,7 @@ import java.util.Properties;
 @OpenAPIDefinition(
         info = @Info(
                 title = "dbpopd",
-                version = "0.1.1"
+                version = "0.1.2"
         )
 )
 public class Application {
