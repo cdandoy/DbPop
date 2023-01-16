2171c4da8f63ff69e36d669bd2d9fb27fd767f57
cdandoy <cedric@dandoy.org>
2023-01-16 15:43:13 -0600

    Use the cached metadata in more places

--- dbpopd/src/main/java/org/dandoy/dbpopd/DatabaseController.java
+++ dbpopd/src/main/java/org/dandoy/dbpopd/DatabaseController.java
@@ -5,42 +5,42 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 import org.dandoy.dbpop.database.*;
+import org.dandoy.dbpopd.database.DatabaseService;
 
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
 
 @Controller("/database")
 public class DatabaseController {
     private final ConfigurationService configurationService;
+    private final DatabaseService databaseService;
 
-    public DatabaseController(ConfigurationService configurationService) {
+    public DatabaseController(ConfigurationService configurationService,
+                              DatabaseService databaseService) {
         this.configurationService = configurationService;
+        this.databaseService = databaseService;
     }
 
-    @Get("search")
+    @Get("" +
+         "search")
     public List<SearchTableResponse> search(String query) {
         configurationService.assertSourceConnection();
 
-        try (Database sourceDatabase = configurationService.createSourceDatabase()) {
-            Set<TableName> tableNames = sourceDatabase.searchTable(query);
-            Collection<Table> tables = sourceDatabase.getTables(tableNames);
-            return tables.stream()
-                    .map(table -> new SearchTableResponse(
-                            table.tableName().toQualifiedName(),
-                            table.tableName(),
-                            table.columns().stream().map(Column::getName).toList(),
-                            table.indexes().stream()
-                                    .map(index -> new SearchTableSearchByResponse(
-                                            "%s (%s)".formatted(index.getName(), String.join(", ", index.getColumns())),
-                                            index.getColumns()
-                                    ))
-                                    .toList()
-                    ))
-                    .sorted(Comparator.comparing(searchTableResponse -> searchTableResponse.tableName().toQualifiedName().length()))
-                    .toList();
-        }
+        return databaseService.getSourceTables()
+                .stream().filter(it -> it.tableName().toQualifiedName().contains(query))
+                .map(table -> new SearchTableResponse(
+                        table.tableName().toQualifiedName(),
+                        table.tableName(),
+                        table.columns().stream().map(Column::getName).toList(),
+                        table.indexes().stream()
+                                .map(index -> new SearchTableSearchByResponse(
+                                        "%s (%s)".formatted(index.getName(), String.join(", ", index.getColumns())),
+                                        index.getColumns()
+                                ))
+                                .toList()
+                ))
+                .sorted(Comparator.comparing(searchTableResponse -> searchTableResponse.tableName().toQualifiedName().length()))
+                .toList();
     }
 
     @Post("dependencies")
