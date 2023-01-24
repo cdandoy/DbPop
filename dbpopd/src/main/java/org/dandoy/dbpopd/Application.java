91d77a98d65899f6f417ece8cbbf5a082394f8e2
cdandoy <cedric@dandoy.org>
2023-01-24 04:18:14 -0600

    Bumped the version

--- dbpopd/src/main/java/org/dandoy/dbpopd/Application.java
+++ dbpopd/src/main/java/org/dandoy/dbpopd/Application.java
@@ -15,7 +15,7 @@ import java.util.Properties;
 @OpenAPIDefinition(
         info = @Info(
                 title = "dbpopd",
-                version = "0.2.4"
+                version = "0.2.5"
         )
 )
 public class Application {
