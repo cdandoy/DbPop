e6073961987fd6572486e9b3c90e0859e7324ce6
cdandoy <cedric@dandoy.org>
2023-01-27 07:52:06 -0600

    Bumped the version. Fixed the tests.

--- dbpopd/src/main/java/org/dandoy/dbpopd/Application.java
+++ dbpopd/src/main/java/org/dandoy/dbpopd/Application.java
@@ -15,7 +15,7 @@ import java.util.Properties;
 @OpenAPIDefinition(
         info = @Info(
                 title = "dbpopd",
-                version = "0.2.6"
+                version = "0.2.7"
         )
 )
 public class Application {
