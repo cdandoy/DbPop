3a76b127d4dc6bf2c165d069f12f6eaec168d4f3
cdandoy <cedric@dandoy.org>
2023-04-23 16:17:50 -0500

    Fixed the bottom menu

--- dbpopr/src/components/app/BottomMenu.tsx
+++ dbpopr/src/components/app/BottomMenu.tsx
@@ -3,20 +3,22 @@ import React from "react";
 
 export default function BottomMenu() {
     return <>
-        <li>
-            <a href="/swagger-ui/" target="_blank">
-                <SidebarMenu text="API" icons="fa fa-book"/>
-            </a>
-        </li>
-        <li>
-            <a href="https://github.com/cdandoy/DbPop" target="_blank" rel="noreferrer">
-                <SidebarMenu text="GitHub" icons="fa-brands fa-docker"/>
-            </a>
-        </li>
-        <li>
-            <a href="https://hub.docker.com/r/cdandoy/dbpop" target="_blank" rel="noreferrer">
-                <SidebarMenu text="Docker" icons="fa-brands fa-github"/>
-            </a>
-        </li>
+        <ul className={"menu-links"}>
+            <li>
+                <a href="/swagger-ui/" target="_blank">
+                    <SidebarMenu text="API" icons="fa fa-book"/>
+                </a>
+            </li>
+            <li>
+                <a href="https://github.com/cdandoy/DbPop" target="_blank" rel="noreferrer">
+                    <SidebarMenu text="GitHub" icons="fa-brands fa-github"/>
+                </a>
+            </li>
+            <li>
+                <a href="https://hub.docker.com/r/cdandoy/dbpop" target="_blank" rel="noreferrer">
+                    <SidebarMenu text="Docker" icons="fa-brands fa-docker"/>
+                </a>
+            </li>
+        </ul>
     </>
 }
