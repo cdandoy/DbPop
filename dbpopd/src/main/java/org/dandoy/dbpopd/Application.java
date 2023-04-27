c79937aa64933f341a28672f1d39f7297dd5d6e8
cdandoy <cedric@dandoy.org>
2023-04-27 07:05:25 -0500

    release 0.2.15

--- dbpopd/src/main/java/org/dandoy/dbpopd/Application.java
+++ dbpopd/src/main/java/org/dandoy/dbpopd/Application.java
@@ -15,7 +15,7 @@ import java.util.Properties;
 @OpenAPIDefinition(
         info = @Info(
                 title = "dbpopd",
-                version = "0.2.14"
+                version = "0.2.15"
         )
 )
 public class Application {
