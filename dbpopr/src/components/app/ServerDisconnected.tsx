e084519914b590253ce5c3c8a1e4fea583a58d04
cdandoy <cedric@dandoy.org>
2023-05-29 09:54:58 -0500

    Delay the "Connecting" message because it was showing every 5 minutes because of the WS

--- dbpopr/src/components/app/ServerDisconnected.tsx
+++ dbpopr/src/components/app/ServerDisconnected.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./ServerDisconnected.scss"
 
 export function ServerDisconnected() {
+    const [delayed, setDelayed] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => setDelayed(false), 2000);
+    });
+
+    if (delayed) return <></>
     return <>
         <div id={"server-disconnected-component"}>
             <h1>
